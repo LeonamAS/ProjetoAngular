9c6c4bacffe0d11d67872197adc09b38f2e426e0
@LeonamAS <leonam.sales@ba.estudante.senai.br>
2025-07-11 11:57:26 -0300

    Implementação da Api no Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { VehicleData } from './../models/vehicleData.model';
 import { Component, OnInit } from '@angular/core';
 import { CabecalhoComponent } from '../componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from '../componentes/rodape/rodape.component';
@@ -5,6 +6,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { DashboardService } from '../services/dashboard.service';
+import { Veiculo } from '../models/veiculo.model';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -13,18 +18,35 @@ import { DashboardService } from '../services/dashboard.service';
     RodapeComponent, 
     MatCardModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
 
+  vehicles: Veiculo[] = [];
+  selectedVehicle!: Veiculo;
+  vehicleData!: VehicleData;
+
+  selectCarForms = new FormGroup({
+    carId: new FormControl('')
+  })
+
   constructor( private dashboardservice: DashboardService) {}
 
   ngOnInit(): void {
     this.dashboardservice.getVehicles().subscribe((res)=>{
       console.log(res.vehicles)
+      this.vehicles = res.vehicles
+    })
+
+    
+
+    this.selectCarForms.controls.carId.valueChanges.subscribe((id)=> {
+      this.selectedVehicle = this.vehicles[Number(id) -1]
     })
   } 
 }
