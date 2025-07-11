6b56c5ca2e67d423d92980f29d7bdede8a9e47a3
LeonamAS <leonam.sales@ba.estudante.senai.br>
2025-07-10 23:19:33 -0300

    Consertei o CSS

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,23 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CabecalhoComponent } from '../componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from '../componentes/rodape/rodape.component';
-import { CardsComponent } from '../componentes/cards/cards.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { DashboardService } from '../services/dashboard.service';
-
-// interface Food {
-//   value: string;
-//   viewValue: string;
-// }
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     CabecalhoComponent, 
     RodapeComponent, 
-    // CardsComponent, 
     MatCardModule,
     MatInputModule,
     MatSelectModule
